2822b954eeba42ff5491be821c482bc5a80f4149
ChousX <gmanenergy@gmail.com>
2024-07-18 10:59:44 -0400

    added some to window

--- bevy/wayland/src/main.rs
+++ bevy/wayland/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     App::new()
         .insert_resource(ClearColor(GOLD.into()))
         .add_plugins(DefaultPlugins.set(WindowPlugin{
-            primary_window: Window{ title: APP_NAME.into(), ..default()},
+            primary_window: Some(Window{ title: APP_NAME.into(), ..default()}),
             ..default()
         }))
         .run();
