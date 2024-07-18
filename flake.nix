9b2de04c801eb670465b6ef7ec03f387ddff0a2c
ChousX <gmanenergy@gmail.com>
2024-07-18 13:06:52 -0400

    replaced flak.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  description = "My Templates! (Based on Grayson's flake templates)";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = (import nixpkgs) {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      templates = {
+        rust-cargo2nix = {
+          description = "Rust project with cargo2nix";
+          path = ./cargo2nix;
+        };
+
+        bevy-app = {
+          description = "Rust and Bevy";
+          path = ./bevy/app;
+        };
+
+        bevy-wayland = {
+          description = "Bevy with wayland";
+          path = ./bevy/wayland;
+        };
+
+        bevy-plugin = {
+          description = "Bevy Plugin";
+          path = ./bevy/plugin;
+        };
+
+        rust-naersk = {
+          description = "Rust Naersk";
+          path = ./naersk;
+        };
+
+        rust-naersk-nightly = {
+          description = "Rust Naersk Nightly";
+          path = ./naersk-nightly;
+        };
+
+      };
+      devShell = {
+        x86_64-linux = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
+      };
+    };
+}
