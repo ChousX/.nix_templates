b72fd4f2bc5389211441c71da38b0ba5e9a22b7c
ChousX <gmanenergy@gmail.com>
2024-07-18 10:49:50 -0400

    fixed

--- bevy/wayland/src/main.rs
+++ bevy/wayland/src/main.rs
@@ -3,8 +3,8 @@ use changeme::{plugins::*, prelude::*};
 fn main() {
     App::new()
         .insert_resource(ClearColor(GOLD.into()))
-        .add_plugins(DefaultPlugins.set(Window{
-            title: APP_NAME.into(),
+        .add_plugins(DefaultPlugins.set(WindowPlugin{
+            primary_window: Window{ title: APP_NAME.into(), ..default()},
             ..default()
         }))
         .run();
